908469db58615ebe592b0912b22b95ed162095f4
Flynn Duniho <flynn@harveyosity.org>
2024-05-23 02:03:50 -0700

    Update to newest GNOME

---
+++ gtk-3.0/extract.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+declare -a n;
+IFS=$'\n'
+n=($(gresource list "$1"))
+SAVEIFS=$IFS
+IFS=$SAVEIFS
+
+for r in "${n[@]}"
+do
+  # echo ">>>" "$r"
+  mkdir -p "$(dirname "${r/#\/com\/ubuntu\/themes\/Yaru-purple-dark\/3.0\/}")"
+  gresource extract "$1" "${r}" > "${r/#\/com\/ubuntu\/themes\/Yaru-purple-dark\/3.0\/}"
+done
+
