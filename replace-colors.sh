00d831c5c235c18605b155c0ecfd5bfcf89047c9
Flynn Duniho <flynn@harveyosity.org>
2024-05-23 02:35:22 -0700

    Another colour swap

--- replace-colors.sh
+++ replace-colors.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/bash
 
-find . -type f -exec sed -i 's/#7764D8/#3b3366/g' {} \;
-find . -type f -exec sed -i 's/#131313/#000000/g' {} \;
-find . -type f -exec sed -i 's/#1d1d1d/#131313/g' {} \;
-find . -type f -exec sed -i 's/#222222/#111111/g' {} \;
+find . -type f -exec sed -i 's/#3b3366/#3b3366/g' {} \;
+find . -type f -exec sed -i 's/#000000/#000000/g' {} \;
+find . -type f -exec sed -i 's/#000000/#000000/g' {} \;
+find . -type f -exec sed -i 's/#0e0e0e/#0e0e0e/g' {} \;
+find . -type f -exec sed -i 's/#0e0e0e/#0e0e0e/g' {} \;
+find . -type f -exec sed -i 's/#4e4485/#4e4485/g' {} \;
+find . -type f -exec sed -i 's/#000000/#000000/g' {} \;
+find . -type f -exec sed -i 's/#0e0e0e/#0e0e0e/g' {} \;
+find . -type f -exec sed -i 's/#101010/#101010/g' {} \;
+find . -type f -exec sed -i 's/#0e0e0e/#0e0e0e/g' {} \;
+find . -type f -exec sed -i 's/#101010/#101010/g' {} \;
+find . -type f -exec sed -i 's/#101010/#101010/g' {} \;
+find . -type f -exec sed -i 's/#1a1a1a/#1a1a1a/g' {} \;
+find . -type f -exec sed -i 's/#7a71a8/#7a71a8/g' {} \;
+find . -type f -exec sed -i 's/#1d1d1d/#000000/g' {} \;
 find . -type f -exec sed -i 's/#111111/#0e0e0e/g' {} \;
-find . -type f -exec sed -i 's/#8574dc/#4e4485/g' {} \;
-find . -type f -exec sed -i 's/#131313/#000000/g' {} \;
-find . -type f -exec sed -i 's/#2f2f2f/#0e0e0e/g' {} \;
-find . -type f -exec sed -i 's/#393939/#101010/g' {} \;
-find . -type f -exec sed -i 's/#313131/#0e0e0e/g' {} \;
-find . -type f -exec sed -i 's/#3b3b3b/#101010/g' {} \;
-find . -type f -exec sed -i 's/#464646/#101010/g' {} \;
-find . -type f -exec sed -i 's/#4f4f4f/#1a1a1a/g' {} \;
-find . -type f -exec sed -i 's/#a599e5/#7a71a8/g' {} \;
-find . -type f -exec sed -i 's/#262626/#1d1d1d/g' {} \;
