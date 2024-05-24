bfa4aaac407b21b5013a732b6205919ffaecb32a
Flynn Duniho <flynn@harveyosity.org>
2024-05-23 20:28:56 -0700

    Add more colour replacements

--- replace-colors.sh
+++ replace-colors.sh
@@ -16,3 +16,7 @@ find . -type f -exec sed -i 's/#1a1a1a/#1a1a1a/g' {} \;
 find . -type f -exec sed -i 's/#7a71a8/#7a71a8/g' {} \;
 find . -type f -exec sed -i 's/#1d1d1d/#000000/g' {} \;
 find . -type f -exec sed -i 's/#111111/#0e0e0e/g' {} \;
+find . -type f -exec sed -i 's/#2c2c2c/#0e0e0e/g' {} \;
+find . -type f -exec sed -i 's/#272727/#181818/g' {} \;
+find . -type f -exec sed -i 's/#373737/#202020/g' {} \;
+find . -type f -exec sed -i 's/#343434/#181818/g' {} \;
